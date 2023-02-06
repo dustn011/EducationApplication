57aeed635670093b18e586abb64a774cf38af7ce
seok <wat0209@naver.com>
2023-02-06 20:35:02 +0900

    민석_작업중

--- studentClient.py
+++ studentClient.py
@@ -8,39 +8,6 @@ from socket import *
 from threading import *
 from datetime import datetime
 
-
-def api_test():
-    # API
-    # 인증키 저장
-    key = "jAB8gOQ%2BEjRxryPTRcGIWjS6sTl2FCowle%2Bb%2FVaRrcoCuQZTgCIEID85tLqWiPIfuY4%2FzUsqf81dQj6dYuTyYg%3D%3D"
-
-    # 인증키 정보가 들어간 url 저장 (요청)
-    url = f'http://openapi.nature.go.kr/openapi/service/rest/InsectService/isctPrtctList?serviceKey={key}'
-
-    # request 모듈을 이용해서 정보 가져오기(byte형태로 가져와지는듯)
-    content = requests.get(url).content
-    # xmltodict 모듈을 이용해서 딕셔너리화 & 한글화
-    diction = xmltodict.parse(content)
-    print(diction)
-    # 아래 과정은 json 기능을 공부해야 이해할 듯
-    # json.dumps를 이용해서 문자열화(데이터를 보낼때 이렇게 바꿔주면 될듯)
-    json_string = json.dumps(diction, ensure_ascii=False)
-    # 데이터 불러올 때(딕셔너리 형태로 받아옴)
-    json_obj = json.loads(json_string)
-    print(json_obj)
-    # diction 과 json_obj의 차이점이 뭔지 모르겠다
-    # print 결과는 똑같아 보이는데
-
-    # <response> 속 <body> 속 <items> 속 <item> 의 각 요소들
-    # response 딕셔너리의 body 딕셔너리의 items 딕셔너리의 item 딕셔너리의 각 key 요소들의 value값을 가져왔음
-    for item in diction['response']['body']['items']['item']:
-        # print(item)
-        print(item['imgUrl'], item['insctFamilyNm'], item['insctOfnmScnm'],
-              item['insctPcmtt'], item['insctPilbkNo'], item['insctofnmkrlngnm'])
-
-    # 아 벌써 힘들다
-
-
 student_ui = uic.loadUiType("studentUi.ui")[0]
 
 
@@ -199,13 +166,39 @@ class StudentClient(QWidget, student_ui):
         self.client_socket.send(send_exitsocketsignal.encode('utf-8'))  # 연결된 소켓(서버)에 채팅 로그 데이터 보내줌
         self.client_socket.close()
 
+    # api 가져오기
+    def api_test(self):
+        # API
+        # 인증키 저장
+        key = "jAB8gOQ%2BEjRxryPTRcGIWjS6sTl2FCowle%2Bb%2FVaRrcoCuQZTgCIEID85tLqWiPIfuY4%2FzUsqf81dQj6dYuTyYg%3D%3D"
+
+        # 인증키 정보가 들어간 url 저장 (요청)
+        url = f'http://openapi.nature.go.kr/openapi/service/rest/InsectService/isctPrtctList?serviceKey={key}'
+
+        # request 모듈을 이용해서 정보 가져오기(byte형태로 가져와지는듯)
+        content = requests.get(url).content
+        # xmltodict 모듈을 이용해서 딕셔너리화 & 한글화
+        diction = xmltodict.parse(content)
+        print(diction)
+        # 아래 과정은 json 기능을 공부해야 이해할 듯
+        # json.dumps를 이용해서 문자열화(데이터를 보낼때 이렇게 바꿔주면 될듯)
+        json_string = json.dumps(diction, ensure_ascii=False)
+        # 데이터 불러올 때(딕셔너리 형태로 받아옴)
+        json_obj = json.loads(json_string)
+        print(json_obj)
+        # diction 과 json_obj의 차이점이 뭔지 모르겠다
+        # print 결과는 똑같아 보이는데
+
+        # <response> 속 <body> 속 <items> 속 <item> 의 각 요소들
+        # response 딕셔너리의 body 딕셔너리의 items 딕셔너리의 item 딕셔너리의 각 key 요소들의 value값을 가져왔음
+        for item in diction['response']['body']['items']['item']:
+            # print(item)
+            print(item['imgUrl'], item['insctFamilyNm'], item['insctOfnmScnm'],
+                  item['insctPcmtt'], item['insctPilbkNo'], item['insctofnmkrlngnm'])
+
 
 if __name__ == "__main__":
-    api_test()
     app = QApplication(sys.argv)
     studentCl = StudentClient()
     studentCl.show()
     app.exec_()
-    
-# 질문 / 학생 / 제목 / 질문 내용
-# 답변 / 학생 / 제목 / 답변 내용
