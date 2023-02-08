943a992355f16caf8793a2bbc63455a2ab1813e9
seok <wat0209@naver.com>
2023-02-08 20:57:44 +0900

    민석_작업중

--- studentClient.py
+++ studentClient.py
@@ -3,11 +3,14 @@ import requests
 import xmltodict
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+from PyQt5.QtCore import QRegExp
 from PyQt5 import uic
 import sys
 from socket import *
+from datetime import datetime
 from threading import *
 import urllib.request
+import time
 
 student_ui = uic.loadUiType("studentUi.ui")[0]
 
@@ -21,7 +24,7 @@ class StudentClient(QWidget, student_ui):
         super().__init__()
         self.setupUi(self)
         self.client_socket = None
-        self.initialize_socket('10.10.21.102', 6666)
+        self.initialize_socket('10.10.21.129', 6666)
         self.listen_thread()
         self.clPage.setCurrentIndex(5)
         self.account = ''
@@ -52,7 +55,7 @@ class StudentClient(QWidget, student_ui):
         self.goMain.clicked.connect(self.go_main)
         self.goMain_2.clicked.connect(self.go_main)
         self.goMain_3.clicked.connect(self.go_main)
-        self.goMain_4.clicked.connect(self.go_main)
+        self.goMain_5.clicked.connect(self.go_main)
         # 퀴즈 목록 가져오기
         self.showQuestions.clicked.connect(self.show_quiz)
         # 퀴즈 선택하기
@@ -72,6 +75,37 @@ class StudentClient(QWidget, student_ui):
         self.mammaliaList.itemSelectionChanged.connect(self.mammalia_info)
         # 조류 도감
         self.birdList.itemSelectionChanged.connect(self.bird_info)
+
+        # 연수's
+        # 로그인 칸에는 한글만 입력 가능
+        reg_exp = QRegExp("[가-힣]+")
+        validator = QRegExpValidator(reg_exp, self.studentName)
+        self.studentName.setValidator(validator)
+
+        # QnA 질문 입력 버튼
+        self.writingComplete.clicked.connect(self.send_question)
+
+        # 테이블 위젯 너비에 맞추기
+        self.question_table.horizontalHeader().setSectionResizeMode(0, QHeaderView.Fixed)
+        self.question_table.horizontalHeader().setSectionResizeMode(1, QHeaderView.Fixed)
+        self.question_table.horizontalHeader().setSectionResizeMode(2, QHeaderView.Stretch)
+        self.question_table.horizontalHeader().setSectionResizeMode(3, QHeaderView.Fixed)
+        self.question_table.horizontalHeader().resizeSection(0, 30)
+        self.question_table.horizontalHeader().resizeSection(1, 30)
+        self.question_table.horizontalHeader().resizeSection(3, 60)
+        # qna 데이터 저장
+        self.list_qna_data = []
+
+        # 질문 제목 글자수 제한하기
+        self.question_title.setMaxLength(22)
+
+        # 질문 클릭하면 질문, 답변 내역 출력
+        self.question_table.cellClicked.connect(self.show_one_qna)
+
+        # 상담 입력하고 엔터
+        self.send_chat.returnPressed.connect(self.method_send_chat)
+        # 상담 입력하고 버튼 클릭
+        self.btn_send_chat.clicked.connect(self.method_send_chat)
 
     def initialize_socket(self, ip, port):
         # TCP socket을 생성하고 server와 연결
@@ -93,19 +127,88 @@ class StudentClient(QWidget, student_ui):
             self.study_mammalia()
         elif index == 3:
             self.study_bird()
-
-    # QnA 페이지로 이동
-    def go_qna(self):
-        self.clPage.setCurrentIndex(2)
+        self.load_study()
 
     # 퀴즈 페이지로 이동
     def go_quiz(self):
         self.show_quiz()
         self.clPage.setCurrentIndex(3)
 
+    # 연수's
     # 상담 페이지로 이동
     def go_consult(self):
+        # 인덱스 0번에 식별자 'plzGiveChattingLog' 넣어주고 [학생 이름]서버로 전송
+        chat = ['plzGiveChattingLog', self.studentName.text()]
+        send_chatAccess = json.dumps(chat)
+        self.client_socket.send(send_chatAccess.encode('utf-8'))
+        print('서버에 상담 채팅 내역을 요청했습니다')
+        self.chat_list.clear()
         self.clPage.setCurrentIndex(4)
+
+    # 상담 보내기
+    def method_send_chat(self):
+        if not self.send_chat.text():
+            QMessageBox.information(self, '입력오류', '내용을 입력해주세요')
+        else:
+            # 인덱스 0번에 식별자 'plzInsertStudentChat' 넣어주고 [학생 이름, 상담 내용]서버로 전송
+            chat = ['plzInsertStudentChat', self.studentName.text(), self.send_chat.text()]
+            send_chat = json.dumps(chat)
+            self.client_socket.send(send_chat.encode('utf-8'))
+            print('서버에 상담 채팅 내역을 보냈습니다')
+            one_chat = f"[{datetime.now().strftime('%D %T')}]\n[{self.studentName.text()}] : {self.send_chat.text()}"
+            self.chat_list.addItem(one_chat)
+            self.send_chat.clear()
+            self.chat_list.scrollToBottom()
+
+    # 질문 선택하면 질문 내역, 응답 내역 출력
+    def show_one_qna(self):
+        self.show_question_title.clear()
+        self.show_question_content.clear()
+        self.question_dt.clear()
+        self.show_answer.clear()
+        self.answer_dt.clear()
+
+        sele_qna_num = int(self.question_table.selectedItems()[0].text())
+        for i in range(len(self.list_qna_data)):
+            if sele_qna_num == self.list_qna_data[i][0]:
+                self.show_question_title.setText(self.list_qna_data[i][2])
+                self.show_question_content.setText(self.list_qna_data[i][3])
+                self.question_dt.setText(self.list_qna_data[i][4])
+                self.show_answer.setText(self.list_qna_data[i][5])
+                self.answer_dt.setText(self.list_qna_data[i][6])
+                print(self.list_qna_data[i])
+
+    # 질문입력 버튼 누르면 실행할 메서드
+    def send_question(self):
+        if not self.question_title.text():
+            QMessageBox.information(self, '입력오류', '제목을 입력해주세요')
+        elif not bool(self.question_content.toPlainText()):
+            QMessageBox.information(self, '입력오류', '질문을 입력해주세요')
+        else:
+            # 인덱스 0번에 식별자 'plzInsertQuestion' 넣어주고 [질문자 이름, 제목, 질문 내용]서버로 전송
+            question = ['plzInsertQuestion', self.studentName.text(), self.question_title.text(),
+                                  self.question_content.toPlainText()]
+            send_question = json.dumps(question)
+            self.client_socket.send(send_question.encode('utf-8'))
+            print('서버에 질문등록 요청을 보냈습니다')
+            self.question_title.clear()
+            self.question_content.clear()
+            QMessageBox.information(self, '등록 완료', '질문을 등록하셨습니다')
+
+    # 연수's
+    # QnA 페이지로 이동
+    def go_qna(self):
+        # 인덱스 0번에 식별자 'plzGiveQuestionLog' 넣어주고 서버로 요청 전송
+        questionLog = ['plzGiveQuestionLog']
+        send_AccessQuestionLog = json.dumps(questionLog)
+        self.client_socket.send(send_AccessQuestionLog.encode('utf-8'))
+        print('서버에 질문내역 요청을 보냈습니다')
+        self.clPage.setCurrentIndex(2)
+
+        self.list_qna_data = []
+
+        # 테이블 위젯 헤더를 제외하고 한 번 초기화
+        self.question_table.clearContents()
 
     # 로그인
     def log_in(self):
@@ -113,19 +216,18 @@ class StudentClient(QWidget, student_ui):
             QMessageBox.information(self, '입력 오류', '이름을 입력해주세요')
         else:
             # 인덱스 0번에 식별자 'plzCheckAccount' 넣어주고 서버로 전송
-            check_account_signal = ['plzCheckAccount', self.studentName.text()]
-            send_account_signal = json.dumps(check_account_signal)
-            self.client_socket.send(send_account_signal.encode('utf-8'))
+            checkAccountSignal = ['plzCheckAccount', self.studentName.text()]
+            send_accountSignal = json.dumps(checkAccountSignal)
+            self.client_socket.send(send_accountSignal.encode('utf-8'))
             print('서버에 로그인 요청을 보냈습니다')
-            # 유저 이름을 저장하고 퀴즈 목록을 불러옴
             self.account = self.studentName.text()
 
     # 로그아웃
     def log_out(self):
         # 인덱스 0번에 식별자 'plzCheckAccount' 넣어주고 서버로 전송
-        logout_signal = ['plzLogoutAccount', self.studentName.text()]
-        send_logout_signal = json.dumps(logout_signal)
-        self.client_socket.send(send_logout_signal.encode('utf-8'))
+        logoutSignal = ['plzLogoutAccount', self.studentName.text()]
+        send_logoutSignal = json.dumps(logoutSignal)
+        self.client_socket.send(send_logoutSignal.encode('utf-8'))
         print('서버에 로그아웃 요청을 보냈습니다')
 
         self.studentName.clear()
@@ -185,7 +287,23 @@ class StudentClient(QWidget, student_ui):
             # 입력했던 답 지우기
             self.answerText.clear()
 
-    # 멸종 위기 곤충 자료
+    def load_study(self):
+        print("불러오기")
+        print(self.account, self.studyTab.currentIndex())
+        self.client_socket.send(json.dumps(["giveStudy", self.account, self.studyTab.currentIndex()]).encode('utf-8'))
+
+    def study_list(self, index):
+        for i in range(int(index) + 1):
+            if self.studyTab.currentIndex() == 0:
+                self.extincList.addItem(self.extinctions[str(i)][1])
+            elif self.studyTab.currentIndex() == 1:
+                self.insectList.addItem(self.insects[str(i)][1])
+            elif self.studyTab.currentIndex() == 2:
+                self.mammaliaList.addItem(self.mammalias[str(i)][1])
+            elif self.studyTab.currentIndex() == 3:
+                self.birdList.addItem(self.birds[str(i)][1])
+
+    # 멸종 위기 곤충 정리
     def study_extinction(self):
         self.extincList.clear()
         extinc_cont = []
@@ -193,21 +311,24 @@ class StudentClient(QWidget, student_ui):
         url = 'http://openapi.nature.go.kr/openapi/service/rest/InsectService/isctPrtctList?serviceKey=%s' % key
         content = requests.get(url).content
         diction = xmltodict.parse(content)
+        index = 0
         for item in diction['response']['body']['items']['item']:
             url = 'http://openapi.nature.go.kr/openapi/service/rest/InsectService/isctIlstrInfo?serviceKey=' \
                   '%s&q1=%s' % (key, item['insctPilbkNo'])
             content = requests.get(url).content
             extinc = xmltodict.parse(content)['response']['body']['item']
-            self.extincList.addItem(extinc['insctOfnmKrlngNm'])
             extinc_cont.append(extinc['imgUrl'])
+            extinc_cont.append(extinc['insctOfnmKrlngNm'])
             extinc_cont.append(extinc['ordKorNm'])
             extinc_cont.append(extinc['familyKorNm'])
             extinc_cont.append(extinc['genusKorNm'])
             for i in range(0, 9):
                 if isinstance(extinc['cont%d' % (i + 1)], str):
                     extinc_cont.append(extinc['cont%d' % (i + 1)])
-            self.extinctions[extinc['insctOfnmKrlngNm']] = extinc_cont
+            self.extinctions[str(index)] = extinc_cont
             extinc_cont = []
+            index += 1
+        print(self.extinctions)
 
     # 선택한 멸종 위기 곤충의 정보 출력
     def extinc_info(self):
@@ -225,7 +346,7 @@ class StudentClient(QWidget, student_ui):
         for i in range(1, len(self.extinctions[self.extincList.currentItem().text()])):
             self.extincInfo.append(self.extinctions[self.extincList.currentItem().text()][i] + "\n")
 
-    # 곤충 도감 10마리 리스트에 추가
+    # 곤충 도감 10마리 정리
     def study_insect(self):
         self.insectList.clear()
         insect_cont = []
@@ -234,16 +355,17 @@ class StudentClient(QWidget, student_ui):
               '%s&st=1&sw=&numOfRows=10&pageNo=1' % key
         content = requests.get(url).content
         insects_list = xmltodict.parse(content)['response']['body']['items']['item']
+        index = 0
         for insect in insects_list:
             url = 'http://openapi.nature.go.kr/openapi/service/rest/InsectService/isctIlstrInfo?serviceKey=' \
                   '%s&q1=%s' % (key, insect['insctPilbkNo'])
             content = requests.get(url).content
             insect_info = xmltodict.parse(content)['response']['body']['item']
-            self.insectList.addItem(insect_info['insctOfnmKrlngNm'])
             if insect_info['imgUrl'] != 'NONE':
                 insect_cont.append(insect_info['imgUrl'])
             else:
                 insect_cont.append('이미지 없음')
+            insect_cont.append(insect_info['insctOfnmKrlngNm'])
             insect_cont.append(insect_info['ordKorNm'])
             insect_cont.append(insect_info['familyKorNm'])
             if isinstance(insect_info['genusKorNm'], str):
@@ -251,8 +373,10 @@ class StudentClient(QWidget, student_ui):
             for i in range(0, 9):
                 if isinstance(insect_info['cont%d' % (i + 1)], str):
                     insect_cont.append(insect_info['cont%d' % (i + 1)])
-            self.insects[insect['insctOfnmKrlngNm']] = insect_cont
+            self.insects[str(index)] = insect_cont
             insect_cont = []
+            index += 1
+        print(self.insects)
 
     # 선택한 곤충 정보 출력
     def insect_info(self):
@@ -273,7 +397,7 @@ class StudentClient(QWidget, student_ui):
         for i in range(1, len(self.insects[self.insectList.currentItem().text()])):
             self.insectInfo.append(self.insects[self.insectList.currentItem().text()][i] + "\n")
 
-    # 포유류 도감 10마리 리스트에 추가
+    # 포유류 도감 10마리 정리
     def study_mammalia(self):
         self.mammaliaList.clear()
         mammalia_cont = []
@@ -282,24 +406,27 @@ class StudentClient(QWidget, student_ui):
               % key
         content = requests.get(url).content
         mammalias_list = xmltodict.parse(content)['response']['body']['items']['item']
+        index = 0
         for mammalia in mammalias_list:
             url = 'http://apis.data.go.kr/1400119/MammService/mammIlstrInfo?serviceKey=%s&q1=%s' \
                   % (key, mammalia['anmlSpecsId'])
             content = requests.get(url).content
             mammalia_info = xmltodict.parse(content)['response']['body']['item']
-            self.mammaliaList.addItem(mammalia_info['anmlGnrlNm'])
             if mammalia_info['imgUrl'] != 'NONE':
                 mammalia_cont.append(mammalia_info['imgUrl'])
             else:
                 mammalia_cont.append('이미지 없음')
+            mammalia_cont.append(mammalia_info['anmlGnrlNm'])
             mammalia_cont.append(mammalia_info['anmlPhlmKorNm'])
             mammalia_cont.append(mammalia_info['anmlClsKorNm'])
             mammalia_cont.append(mammalia_info['anmlOrdKorNm'])
             mammalia_cont.append(mammalia_info['anmlFmlyKorNm'])
             mammalia_cont.append(mammalia_info['eclgDpftrCont'])
             mammalia_cont.append(mammalia_info['gnrlSpftrCont'])
-            self.mammalias[mammalia_info['anmlGnrlNm']] = mammalia_cont
+            self.mammalias[str(index)] = mammalia_cont
             mammalia_cont = []
+            index += 1
+        print(self.mammalias)
 
     # 선택한 포유류 정보 출력
     def mammalia_info(self):
@@ -320,7 +447,7 @@ class StudentClient(QWidget, student_ui):
         for i in range(1, len(self.mammalias[self.mammaliaList.currentItem().text()])):
             self.mammaliaInfo.append(self.mammalias[self.mammaliaList.currentItem().text()][i] + "\n")
     
-    # 조류 도감 10마리 리스트에 추가
+    # 조류 도감 10마리 정리
     def study_bird(self):
         self.birdList.clear()
         bird_cont = []
@@ -329,24 +456,27 @@ class StudentClient(QWidget, student_ui):
               % key
         content = requests.get(url).content
         birds_list = xmltodict.parse(content)['response']['body']['items']['item']
+        index = 0
         for bird in birds_list:
             url = 'http://apis.data.go.kr/1400119/BirdService/birdIlstrInfo?serviceKey=%s&q1=%s' \
                   % (key, bird['anmlSpecsId'])
             content = requests.get(url).content
             bird_info = xmltodict.parse(content)['response']['body']['item']
-            self.birdList.addItem(bird_info['anmlGnrlNm'])
             if bird_info['imgUrl'] != 'NONE':
                 bird_cont.append(bird_info['imgUrl'])
             else:
                 bird_cont.append('이미지 없음')
+            bird_cont.append(bird_info['anmlGnrlNm'])
             bird_cont.append(bird_info['anmlPhlmKorNm'])
             bird_cont.append(bird_info['anmlClsKorNm'])
             bird_cont.append(bird_info['anmlOrdKorNm'])
             bird_cont.append(bird_info['anmlFmlyKorNm'])
             bird_cont.append(bird_info['eclgDpftrCont'])
             bird_cont.append(bird_info['gnrlSpftrCont'])
-            self.birds[bird_info['anmlGnrlNm']] = bird_cont
+            self.birds[str(index)] = bird_cont
             bird_cont = []
+            index += 1
+        print(self.birds)
     
     # 선택한 조류 정보 출력
     def bird_info(self):
@@ -392,7 +522,6 @@ class StudentClient(QWidget, student_ui):
                 elif identifier == "hereQuiz":
                     # 쓸데없이 3중 리스트로 돼있어서 2중으로 줄임
                     self.questions = message_log[0]
-                    print(self.questions)
                     # 퀴즈 갯수만큼 행 생성
                     self.questionList.setRowCount(len(self.questions))
                     for row in range(len(self.questions)):
@@ -409,6 +538,45 @@ class StudentClient(QWidget, student_ui):
                     self.clPage.setCurrentIndex(0)
                 elif identifier == 'failed_login':
                     self.fail_message.setText('로그인 실패')
+                elif identifier == 'send_qna_data':
+                    self.show_all_qna(message_log)
+                elif identifier == 'send_studentChat_data':
+                    self.show_chattingLog(message_log)
+                elif identifier == 'send_teacher_message':
+                    self.show_teacherMessage(message_log)
+                elif identifier == 'hereStudyLoad':
+                    self.study_list(message_log[0])
+
+    # 선생이 메시지 보내면 메시지 창에 띄우기
+    def show_teacherMessage(self, teacher_message):
+        print(teacher_message)
+        one_chat = f"[{datetime.now().strftime('%D %T')}]\n[{teacher_message[0]}] : {teacher_message[1]}"
+        self.chat_list.addItem(one_chat)
+        time.sleep(0.000000000001)
+        self.chat_list.scrollToBottom()
+
+    # 서버에서 받아온 chatting 데이터 불러오기
+    def show_chattingLog(self, chatting_log):
+        one_chat = f"[{chatting_log[0]}]\n[{chatting_log[1]}] : {chatting_log[2]}"
+        self.chat_list.addItem(one_chat)
+        time.sleep(0.000000000001)
+        self.chat_list.scrollToBottom()
+
+    # 서버에서 받아온 qna데이터 불러오기
+    def show_all_qna(self, qna_data):
+        self.list_qna_data.append(qna_data)
+
+        # 테이블 위젯 헤더를 제외하고 한 번 초기화
+        self.question_table.clearContents()
+
+        # 테이블 위젯의 row 개수 정해주기
+        self.question_table.setRowCount(len(self.list_qna_data))
+
+        for i in range(len(self.list_qna_data)):
+            self.question_table.setItem(i, 0, QTableWidgetItem(str(self.list_qna_data[i][0])))
+            self.question_table.setItem(i, 1, QTableWidgetItem(str(self.list_qna_data[i][1])))
+            self.question_table.setItem(i, 2, QTableWidgetItem(str(self.list_qna_data[i][2])))
+            self.question_table.setItem(i, 3, QTableWidgetItem(str(self.list_qna_data[i][7])))
 
     # 유저가 종료했을 경우 (함수를 따로 실행 안해도 종료하면 알아서 실행됨)
     def closeEvent(self, QCloseEvent):
